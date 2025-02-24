4e34e082e04f09fcd2fae470e3bdd7dab01c4f51
ganesh <ganeshsatpute1999>
2025-02-24 17:55:12 +0530

    profile part deleted

--- lib/domain/repository/profile_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:food_app/core/resources/failures.dart';
-import 'package:food_app/domain/entities/profile_entity.dart';
-
-abstract class ProfileRepository {
-  Future<Either<Failure, Profile>> getProfile();
-}
