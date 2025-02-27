d62a9c0a08b75994767335591e36c027f36a7c28
Hrishikesh Kedar <hrishikeshkedar30401@gmail.com>
2025-02-27 16:39:15 +0530

    final home screen, profile screen, authentication, favorites screen, login, bottom navigation widget, login button and signup button widget and resp ui and bloc

--- lib/domain/repository/search_recipe_repository.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:dartz/dartz.dart';
-import 'package:food_app/core/resources/failures.dart';
-import 'package:food_app/domain/entities/search_recipe_entity.dart';
-
-abstract class SearchRecipeRepository {
-  Future<Either<Failure, List<SearchRecipeEntity>>> searchRecipes(String query);
-}
